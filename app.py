02869a411515b689bfd6835e38675ed774fdfc0f
omarRmalik <maliko@mail.gvsu.edu>
2024-05-22 10:24:52 -0400

    1st commit 5-22-2024

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ national = (
     )
 )
 
-print(national['answer'].unique())
+
 # Instantiate app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY], meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}])
 
