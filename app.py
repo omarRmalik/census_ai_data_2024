96ae77deb2469789b87ad0f3d739cf1025ed7e1c
omarRmalik <maliko@mail.gvsu.edu>
2024-05-22 10:54:31 -0400

    2nd commit 5-22-2024

--- app.py
+++ app.py
@@ -10,6 +10,21 @@ import numpy as np
 sheets = ['National Response Estimates', 'Sector Response Estimates', 'State Response Estimates', 'Employment Response Estimates']
 df = pd.read_excel('C:/Users/ormal/Documents/ai_data_census/ai_supplement_us_census.xlsx', sheet_name=sheets)
 
+# Question map
+
+question_map = {'In the last six months, what types or applications of Artificial Intelligence (AI) did this business use in producing goods or services?':
+                'What types of applications of AI did this business use?',
+                'In the last six months, to use Artificial Intelligence (AI), what changes did this business make?':
+                'What changes did this business make to use AI?',
+                'In the last six months, did this business use Artificial Intelligence to perform operations previously performed by existing equipment or software in producing goods or services?':
+                'Has AI use substituted existing software/equipment in operations?',
+                'During the next six months, to use Artificial Intelligence, what changes do you think this business will make?':
+                'What changes do you intend to make to use AI?',
+                'During the next six months, what types or applications of Artificial Intelligence (AI) do you think this business will use in producing goods or services?':
+                'What types of applications of AI do you intend to use?',
+                'During the next six months, do you think this business will use Artificial Intelligence to perform operations currently performed by existing equipment and software in producing goods or services?':
+                'Will AI substitute existing software/equipment in current operations in the future?'}
+
 # National data cleanup
 national = (
     df['National Response Estimates']
@@ -18,12 +33,50 @@ national = (
     .rename(columns=lambda c: c.lower().replace(' ', '_'))
     .sort_values(by='estimate', ascending=False)
     .assign(
+        question=lambda df_: df_['question'].map(question_map),
         answer=lambda df_: df_['answer'].astype('str'),
         estimate=lambda df_: df_['estimate'].str.replace('%', '').astype('float'),
         standard_error=lambda df_: df_['standard_error'].str.replace('%', '').astype('float'),
     )
 )
 
+# naics_codes
+
+naics_codes = pd.read_excel('https://www.census.gov/naics/2022NAICS/2022_NAICS_Descriptions.xlsx')
+
+# naics codes cleanup
+
+naics_codes = (
+    naics_codes
+      .assign(
+          sector = lambda df_: df_['Code'].astype('str'),
+          title = lambda df_: df_['Title'].str.replace('T$','', regex=True).str.replace('and', '&')
+      )
+      .loc[lambda df_: df_['sector'].str.len() == 2]
+      .drop(['Code', 'Title', 'Description'], axis='columns')
+      .reset_index(drop=True)
+)
+
+# Sector data cleanup
+
+sector = (df['Sector Response Estimates']
+          .loc[lambda df_: df_['Question'].str.contains('applications|operations|changes')]
+          .drop(['Scope (see data dictionary)', 'Question ID', 'Answer ID'], axis='columns')
+          .replace('S', np.nan)
+          .dropna()
+          .rename(columns=lambda c: c.lower().replace(' ', '_'))
+          .sort_values(by='estimate', ascending=False)
+          .assign(
+               question = lambda df_: df_['question'].map(question_map),
+               sector = lambda df_: df_['sector'].astype('str'),
+               answer = lambda df_: df_['answer'].astype('str'),
+               estimate=lambda df_: df_['estimate'].str.replace('%', '').astype('float'),
+               standard_error=lambda df_: df_['standard_error'].str.replace('%', '').astype('float')
+               )
+          .pipe((lambda df_: pd.merge(naics_codes, df_, on='sector')))
+          .rename(columns = {'title': 'industry'})
+          .drop('sector', axis='columns')
+)
 
 # Instantiate app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY], meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}])
@@ -55,7 +108,8 @@ app.layout = dbc.Container([
     ])
 ])
 
-# Define callback to update bar chart based on selected question
+# National bar chart callback
+
 @app.callback(
     Output('graph-card', 'children'),
     [Input('question-dropdown', 'value')]
